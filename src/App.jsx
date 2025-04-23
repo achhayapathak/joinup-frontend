10d032f6d9011e9067d68225c168364ec62f36ac
achhayapathak <achhayapathak11@gmail.com>
2025-04-23 19:41:55 +0530

    v1.2.0: Testing

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<Login />} />
     </Routes>
   )
 }
